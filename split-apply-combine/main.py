f4fb2e03b5d81ddcedb003f2dbf79fc2bd0533d5
Lisias (Lee) Loback <lisias@loback.co.uk>
2024-07-19 13:50:35 +0100

    initial data for exercise

--- split-apply-combine/main.py
+++ split-apply-combine/main.py
@@ -5,6 +5,15 @@
 # Creating the DataFrame
 #   A Pandas DataFrame is a 2 dimensional data structure, like a 2 dimensional
 #   array, or a table with rows and columns.
+data = {
+    'animal': ['falcon', 'parrot', 'lion', 'monkey', 'leopard'],
+    'class': ['bird', 'bird', 'mammal', 'mammal', 'mammal'],
+    'order': [
+        'Falconiformes', 'Psittaciformes', 'Carnivora',
+        'Primates', 'Carnivora'
+    ],
+    'max_speed': [389.0, 24.0, 80.2, None, 58.09]
+}
 
 
 # Split-Apply-Combine
